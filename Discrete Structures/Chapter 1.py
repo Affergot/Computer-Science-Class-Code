7f363d8f364f9987760513508d00c93347cbdb54
Affergot <ralphsartin721@gmail.com>
2023-01-18 03:40:43 -0600

    Finished Bit Operator functions

--- Discrete Structures/Chapter 1.py
+++ Discrete Structures/Chapter 1.py
@@ -1,4 +1,28 @@
-'''
-Write a program: Given two bit strings of length n, find the bitwise AND,
-bitwise OR, and bitwise XOR of these strings.
-'''
+import time
+
+OperatorsList = ["AND","OR","XOR"]
+
+num1 = int(input("Enter first number:\n"))
+num2 = int(input("Enter second number:\n"))
+bin1 = bin(num1)[2:].zfill(8)
+bin2 = bin(num2)[2:].zfill(8)
+
+operator = input(f"Enter operator {OperatorsList}:\n").upper()
+
+if operator == "AND":
+    result = num1 & num2
+elif operator == "OR":
+    result = num1 | num2
+elif operator == "XOR":
+    result = num1 ^ num2
+else:
+    print("Invalid operator")
+    result = None
+time.sleep(1)
+if result is not None:
+    print(f'{num1}: {bin1}')
+    print(f'{operator}')
+    print(f'{num2}: {bin2}')
+    time.sleep(2)
+    print("Result in binary:", bin(result)[2:].zfill(8))
+    print("Result in decimal:", result)    
