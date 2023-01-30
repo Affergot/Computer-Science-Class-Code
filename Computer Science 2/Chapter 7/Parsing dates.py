41a271e1234b60a22e8cd04d3a487923ca307966
Affergot <ralphsartin721@gmail.com>
2023-01-30 00:02:43 -0600

    Started a file for Chapter 7 hw assignment

---
+++ Computer Science 2/Chapter 7/Parsing dates.py
@@ -0,0 +1,7 @@
+'''
+Write a program to read dates from input, one date per line.
+Each date's format must be as follows: March 1, 1990
+Any date not following that format is incorrect and should be ignored.
+The input ends with -1 on a line alone.
+Output each correct date as: 3/1/1990.
+'''
