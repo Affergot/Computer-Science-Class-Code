085ceaab927f13af963d916998b31f0abc0c2ce0
Affergot <ralphsartin721@gmail.com>
2023-02-22 23:36:24 -0600

    Finished Chapter 6

---
+++ Discrete Structures/Chapter 6.py
@@ -0,0 +1,11 @@
+from math import comb
+from CharByChar import *
+
+charByChar("How many types of cookies are there?\n:")
+num_cookie_types = int(input())
+charByChar("How many cookies do you want to choose?\n:")
+num_cookies_to_choose = int(input())
+
+possible_combinations = comb(num_cookie_types + num_cookies_to_choose - 1, num_cookies_to_choose)
+
+charByChar(f"There are {possible_combinations} ways to choose {num_cookies_to_choose} cookies from {num_cookie_types} types of cookies.")
