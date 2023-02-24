bd80fd9d2aff2bca1e4604c06afd9af8c18ed2b9
Affergot <ralphsartin721@gmail.com>
2023-02-24 12:48:33 -0600

    Prepped for CS2 Chapter 9

---
+++ Computer Science 2/Chapter 9/Rolling_for_x.py
@@ -0,0 +1,44 @@
+'''
+Complete function roll_specific_number() that has three parameters: a GVDie object, an integer representing a desired face number of a die,
+ and an integer representing the goal amount of times to roll the desired face number. The function roll_specific_number() then rolls the die
+   until the desired face number is rolled the goal amount of times and returns the number of rolls required.
+
+Note: For testing purposes, a GVDie object is created in the main() function using a pseudo-random number generator with a fixed seed value.
+ The program uses a seed value of 15 during development, but when submitted, a different seed value will be used for each test case.
+'''
+import random
+from TypedOut import *
+
+class GVDie:
+    def __init__(self):
+        self.my_value = random.randint(1, 6)
+        self.rand = random.Random()
+
+    def roll(self):
+        self.my_value = self.rand.randint(1, 6)
+
+    def set_seed(self, seed):
+        self.rand.seed(seed)
+    
+    def roll_specific_number(self, selected_face, taget_amount):
+        rolls = 0
+        while True:
+            self.roll()
+            rolls += 1
+            if self.my_value == selected_face:
+                taget_amount -= 1
+            if taget_amount == 0:
+                return rolls
+
+
+if __name__ == "__main__":
+
+    die = GVDie()
+    die.set_seed(15)
+    charByChar("See how many times it takes to roll a specific number on a die.")
+    charByChar("Enter the desired face value\n")
+    selected_face = int(input(":"))
+    charByChar("Enter the goal amount of times to roll the desired face value\n")
+    taget_amount = int(input(":"))
+    rolls = die.roll_specific_number(selected_face, taget_amount)
+    charByChar(f"It took {rolls} rolls to roll {taget_amount} {selected_face}'s.")
--- Computer Science 2/Chapter 9/Triangle_Comparison.py
+++ Computer Science 2/Chapter 9/Triangle_Comparison.py
@@ -42,3 +42,11 @@ if __name__ == "__main__":
         charByChar(f"The first triangle is {area_difference} units larger than the second triangle with a total area of {triangle1_area}.")
     elif area_difference < 0:
         charByChar(f"The second triangle is {abs(area_difference)} units larger than the first triangle with a total area of {triangle2_area}.")
+    time.sleep(1)
+
+    charByChar("\nTriangle with smaller area:\n")
+    print("Base:", triangle1.base if triangle1_area < triangle2_area else triangle2.base)
+    time.sleep(1)
+    print("Height:", triangle1.height if triangle1_area < triangle2_area else triangle2.height)
+    time.sleep(1)
+    print("Area:", triangle1_area if triangle1_area < triangle2_area else triangle2_area)
